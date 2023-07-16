44abdd52cc7a9d9954db859dc37f9c35426cce5e
Rochell Hill <rrhill6@yahoo.com>
2023-07-16 14:47:29 -0500

    Update script.js
    completed file

--- script.js
+++ script.js
@@ -2,15 +2,35 @@
 
 window.addEventListener("load", function() {
 
-   let listedPlanets;
+    let listedPlanets;
    // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-   let listedPlanetsResponse;
+   let listedPlanetsResponse = myFetch();
    listedPlanetsResponse.then(function (result) {
        listedPlanets = result;
        console.log(listedPlanets);
    }).then(function () {
        console.log(listedPlanets);
        // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
+        let chosenPlanet = pickPlanet(listedPlanets);
+
+        addDestinationInfo(document, chosenPlanet.name, chosenPlanet.diameter, chosenPlanet.star, 
+            chosenPlanet.distance, chosenPlanet.moons, chosenPlanet.image);
    })
-   
+
+        let form = document.querySelector("form");
+        
+        form.addEventListener("submit", function(event) {
+            event.preventDefault();
+
+            let pilotNameInput = document.querySelector("input[name=pilotName]");
+            let copilotNameInput = document.querySelector("input[name=copilotName]");
+            let fuelLevelInput = document.querySelector("input[name=fuelLevel]");
+            let cargoMassInput = document.querySelector("input[name=cargoMass]");
+            let list = document.getElementById("faultyItems");
+            list.style.visibility = "hidden";
+
+            formSubmission(document, list, pilotNameInput.value, copilotNameInput.value, 
+                fuelLevelInput.value, cargoMassInput.value);
+        });
+
 });
